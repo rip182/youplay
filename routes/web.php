e77bf30da3d91e863eaa01bf8f34861d99f0afab
unknown <ghostproject50@gmail.com>
2019-04-10 14:45:03 +0800

    index/landing as been added

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','indexController@index');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
